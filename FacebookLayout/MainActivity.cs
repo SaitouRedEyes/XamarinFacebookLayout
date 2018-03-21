8fbcf34098a8fa061c03a2e27ad6a62c20af8142
SaitouRedEyes <zeca.saitou@gmail.com>
2018-03-21 18:55:03 -0300

    Facebook Login Layout Example
    This project show a example of a layout usage in an Android App.

---
+++ FacebookLayout/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+namespace FacebookLayout
+{
+    [Activity(Label = "FacebookLayout", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private EditText editLogin;
+        private EditText editPass;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Main);
+
+            Button buttonLogin = FindViewById<Button>(Resource.Id.button_login);
+            editLogin = FindViewById<EditText>(Resource.Id.edit_login);
+            editPass = FindViewById<EditText>(Resource.Id.edit_pass);
+
+            buttonLogin.Click += OnButtonLoginClicked;
+        }
+
+        private void OnButtonLoginClicked(object sender, EventArgs e)
+        {
+            if(!editLogin.Text.Equals("") && !editPass.Text.Equals(""))
+            {
+                Toast.MakeText(this, "Usuário logado com sucesso!", ToastLength.Long).Show();
+            }
+            else
+            {
+                Toast.MakeText(this, "Faltam informações para log in", ToastLength.Long).Show();
+            }
+        }
+    }
+}
+
